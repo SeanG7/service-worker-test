f81d2d10ca706c925fe4ccb69c5e642b39ced48e
Sean <gilmore.seven@gmail.com>
2016-09-17 14:29:12 +1000

    added caching

--- sw.js
+++ sw.js
@@ -16,6 +16,19 @@ self.addEventListener('install', function(event) {
       })
   );
 });
+
+self.addEventListener('fetch', function(event){
+  event.respondWith(
+    caches.match(event.request).then(function(response){
+      //cache hit - return response
+      if (response){
+        return response;
+      }
+      return fetch(event.request);
+    })
+  );
+})
+
 self.addEventListener('activate', function(event) {
   console.log('Activated', event);
 });
